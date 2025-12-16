17772d7cf45a59ad963dadcc019a86eac92d86e6
azaanazam <azanazam@gmail.com>
2025-12-16 09:54:07 +0500

    Update App.js with new changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [username, setUsername] = useState("");
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    if (username === "") return;
+
+    fetch(`https://api.github.com/users/${username}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setUserData(data);
+      });
+  }, [username]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" placeholder="GitHub username" onChange={(e) => setInputValue(e.target.value)}/>
+
+      <button onClick={() => setUsername(inputValue)}>Search</button>
+
+      {userData && (
+        <div className="card">
+          <img src={userData.avatar_url} alt=""/>
+          <h2>{userData.name}</h2>
+          <p>Followers: {userData.followers}</p>
+          <p>Following: {userData.following}</p>
+          <p>Repos: {userData.public_repos}</p>
+        </div>
+      )}
     </div>
   );
 }
